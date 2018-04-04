cec25b613b91df06a97c56a94b8154c810f77f1c
tvrtko <tvrtkom@gmail.com>
2018-04-04 23:32:15 +0200

    fixes

--- shellsetup.sh
+++ shellsetup.sh
@@ -4,17 +4,12 @@ sudo su -c '
 apt-get install aptitude
 aptitude update
 aptitude upgrade
-aptitude install zsh git cmake vim python python-pip python3 cargo vim emacs
+aptitude install zsh git zlib1g-dev cmake vim python python-pip python3 cargo vim vim-gtk emacs terminator
 '
 
 my_dot_zsh='https://gist.githubusercontent.com/TvrtkoM/0904a88778a7dfdb2bc9816f03db30f3/raw/072509485796d1c7d614831b397705f9e866e6af/.zshrc_home';
 my_dot_vimrc='https://gist.githubusercontent.com/TvrtkoM/79328746f59926d820e288fa68f41ac6/raw/cbff5cee9f5190eae7a5135ba93d314818fc9805/.vimrc_home';
-my_dot_spacemacs='https://gist.github.com/TvrtkoM/7a0098cfd3ae408569cd200acaa7d766';
-
-# download dotfiles to home
-wget $my_dot_zsh -O $HOME/.zshrc
-wget $my_dot_vimrc -O $HOME/.vimrc
-wget $my_doc_spacemacs -O $HOME/.spacemacs
+my_dot_spacemacs='https://gist.githubusercontent.com/TvrtkoM/7a0098ce408569cd200acaa7d766/raw/617ae1dd04a22ae3d35bbe973fc2a8b5fc0e2670/.spacemacs';
 
 src_dir="$HOME/src"
 virtualenv_dir="$HOME/pyvenvs";
@@ -36,11 +31,16 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 nvm install stable
 
+# download dotfiles to home
+wget $my_dot_zsh -O $HOME/.zshrc
+wget $my_dot_vimrc -O $HOME/.vimrc
+wget $my_doc_spacemacs -O $HOME/.spacemacs
+
 # set up python3 virtualenv
 sudo python -m pip install virtualenv
 virtualenv -p python3 $def_virtualenv_dir
 
-source $def_virtualenv_dir/py3/bin/activate
+source $def_virtualenv_dir/bin/activate
 pip install ipython
 deactivate # deactivate virtual env py3
 
